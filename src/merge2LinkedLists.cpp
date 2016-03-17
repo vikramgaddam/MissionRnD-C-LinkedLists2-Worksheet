380da395719c1f9638e763ee1d208404abd249a9
vikramgaddam <vikramgaddam@missionrnd.com>
2016-03-18 02:10:07 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,53 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
+	if ((head1 == NULL) && (head2==NULL))
 	return NULL;
+	if (head1 == NULL)
+		return head2;
+
+	if (head2 == NULL)
+		return head1;
+
+	struct node *res = head1;
+	struct node *temp1, *temp2;
+	temp1 = head1;
+	temp2 = head2;
+
+	if (head1->num < head2->num)
+	{
+		res = head1;
+		head1 = head1->next;
+	}
+	else
+	{
+		res = head2;
+		head2 = head2->next;
+	}
+	temp1 = res;
+	while ((head1 != NULL) && (head2 != NULL))
+	{
+		if (head1->num < head2->num)
+		{
+			res->next = head1;
+			res = res->next;
+			head1 = head1->next;
+		}
+		else
+		{
+			res->next = head2;
+			res = res->next;
+			head2 = head2->next;
+		}
+	}
+
+	if (head1 != NULL)
+	{
+		res->next = head1;
+	}
+	if (head2 != NULL)
+	{
+		res->next = head2;
+	}
+	return temp1;
 }
